b10f372bd403a61c16428e96a9df092f70d5b22e
mattewo <mattewo@MATTHEWOMAGENE>
2023-03-01 14:35:53 -0800

    Add project files.

---
+++ TestApp/MainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace TestApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDrugCount_1 = new System.Windows.Forms.Label();
+            this.lblDrugName_1 = new System.Windows.Forms.Label();
+            this.btnDrug_1 = new System.Windows.Forms.Button();
+            this.btnDrug_2 = new System.Windows.Forms.Button();
+            this.lblDrugName_2 = new System.Windows.Forms.Label();
+            this.lblDrugCount_2 = new System.Windows.Forms.Label();
+            this.btnDrug_3 = new System.Windows.Forms.Button();
+            this.lblDrugName_3 = new System.Windows.Forms.Label();
+            this.lblDrugCount_3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDrugCount_1
+            // 
+            this.lblDrugCount_1.AutoSize = true;
+            this.lblDrugCount_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDrugCount_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDrugCount_1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblDrugCount_1.Location = new System.Drawing.Point(89, 59);
+            this.lblDrugCount_1.Name = "lblDrugCount_1";
+            this.lblDrugCount_1.Size = new System.Drawing.Size(71, 75);
+            this.lblDrugCount_1.TabIndex = 0;
+            this.lblDrugCount_1.Text = "0";
+            // 
+            // lblDrugName_1
+            // 
+            this.lblDrugName_1.AutoSize = true;
+            this.lblDrugName_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDrugName_1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblDrugName_1.Location = new System.Drawing.Point(56, 145);
+            this.lblDrugName_1.Name = "lblDrugName_1";
+            this.lblDrugName_1.Size = new System.Drawing.Size(134, 20);
+            this.lblDrugName_1.TabIndex = 1;
+            this.lblDrugName_1.Text = "Acetaminophen";
+            this.lblDrugName_1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnDrug_1
+            // 
+            this.btnDrug_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnDrug_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDrug_1.Location = new System.Drawing.Point(77, 179);
+            this.btnDrug_1.Name = "btnDrug_1";
+            this.btnDrug_1.Size = new System.Drawing.Size(92, 33);
+            this.btnDrug_1.TabIndex = 2;
+            this.btnDrug_1.Text = "Add One";
+            this.btnDrug_1.UseVisualStyleBackColor = false;
+            // 
+            // btnDrug_2
+            // 
+            this.btnDrug_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnDrug_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDrug_2.Location = new System.Drawing.Point(329, 179);
+            this.btnDrug_2.Name = "btnDrug_2";
+            this.btnDrug_2.Size = new System.Drawing.Size(92, 33);
+            this.btnDrug_2.TabIndex = 5;
+            this.btnDrug_2.Text = "Add One";
+            this.btnDrug_2.UseVisualStyleBackColor = false;
+            // 
+            // lblDrugName_2
+            // 
+            this.lblDrugName_2.AutoSize = true;
+            this.lblDrugName_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDrugName_2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblDrugName_2.Location = new System.Drawing.Point(337, 145);
+            this.lblDrugName_2.Name = "lblDrugName_2";
+            this.lblDrugName_2.Size = new System.Drawing.Size(77, 20);
+            this.lblDrugName_2.TabIndex = 4;
+            this.lblDrugName_2.Text = "Oxycotin";
+            this.lblDrugName_2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDrugCount_2
+            // 
+            this.lblDrugCount_2.AutoSize = true;
+            this.lblDrugCount_2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDrugCount_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDrugCount_2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblDrugCount_2.Location = new System.Drawing.Point(341, 59);
+            this.lblDrugCount_2.Name = "lblDrugCount_2";
+            this.lblDrugCount_2.Size = new System.Drawing.Size(71, 75);
+            this.lblDrugCount_2.TabIndex = 3;
+            this.lblDrugCount_2.Text = "0";
+            // 
+            // btnDrug_3
+            // 
+            this.btnDrug_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnDrug_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDrug_3.Location = new System.Drawing.Point(577, 179);
+            this.btnDrug_3.Name = "btnDrug_3";
+            this.btnDrug_3.Size = new System.Drawing.Size(92, 33);
+            this.btnDrug_3.TabIndex = 8;
+            this.btnDrug_3.Text = "Add One";
+            this.btnDrug_3.UseVisualStyleBackColor = false;
+            // 
+            // lblDrugName_3
+            // 
+            this.lblDrugName_3.AutoSize = true;
+            this.lblDrugName_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDrugName_3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblDrugName_3.Location = new System.Drawing.Point(582, 145);
+            this.lblDrugName_3.Name = "lblDrugName_3";
+            this.lblDrugName_3.Size = new System.Drawing.Size(87, 20);
+            this.lblDrugName_3.TabIndex = 7;
+            this.lblDrugName_3.Text = "Ibuprofen";
+            this.lblDrugName_3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDrugCount_3
+            // 
+            this.lblDrugCount_3.AutoSize = true;
+            this.lblDrugCount_3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDrugCount_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDrugCount_3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblDrugCount_3.Location = new System.Drawing.Point(589, 59);
+            this.lblDrugCount_3.Name = "lblDrugCount_3";
+            this.lblDrugCount_3.Size = new System.Drawing.Size(71, 75);
+            this.lblDrugCount_3.TabIndex = 6;
+            this.lblDrugCount_3.Text = "0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(761, 450);
+            this.Controls.Add(this.btnDrug_3);
+            this.Controls.Add(this.lblDrugName_3);
+            this.Controls.Add(this.lblDrugCount_3);
+            this.Controls.Add(this.btnDrug_2);
+            this.Controls.Add(this.lblDrugName_2);
+            this.Controls.Add(this.lblDrugCount_2);
+            this.Controls.Add(this.btnDrug_1);
+            this.Controls.Add(this.lblDrugName_1);
+            this.Controls.Add(this.lblDrugCount_1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDrugCount_1;
+        private System.Windows.Forms.Label lblDrugName_1;
+        private System.Windows.Forms.Button btnDrug_1;
+        private System.Windows.Forms.Button btnDrug_2;
+        private System.Windows.Forms.Label lblDrugName_2;
+        private System.Windows.Forms.Label lblDrugCount_2;
+        private System.Windows.Forms.Button btnDrug_3;
+        private System.Windows.Forms.Label lblDrugName_3;
+        private System.Windows.Forms.Label lblDrugCount_3;
+    }
+}
+
